48f5a0905e8147d00f55b1df43922605dd79a698
Glo <olsheaolufsen@gmail.com>
2024-01-26 20:38:50 +0100

    Add classnames to button

--- src/components/buttons/PrimaryButton.jsx
+++ src/components/buttons/PrimaryButton.jsx
@@ -1,7 +1,16 @@
-export default function PrimaryButton({ children, onClick, ...props }) {
-    return (
-        <button onClick={onClick} className="bg-primary text-white rounded-full py-4 px-8" {...props}>
-            {children}
-        </button>
-    )
+import classNames from 'classnames';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function PrimaryButton({ children, className, onClick, ...props }) {
+  return (
+    <button onClick={onClick} className={classNames(
+      "text-white rounded-full py-3 px-6 md:py-4 md:px-10 bg-gradient-to-br hover:bg-gradient-to-r transition-all duration-300 from-primary to-secondary hover:bg-blend-darken",
+      className
+    )}
+      {...props}
+    >
+      {children}
+    </button >
+  );
 }
--- src/components/buttons/SecondaryButton.jsx
+++ src/components/buttons/SecondaryButton.jsx
@@ -1,7 +1,19 @@
-export default function SecondaryButton({ children, onClick, ...props }) {
-    return (
-        <button onClick={onClick} className="border border-primary text-primary rounded-full py-4 px-8" {...props}>
-            {children}
-        </button>
-    )
+import classNames from 'classnames';
+import React from 'react';
+import PropTypes from 'prop-types';
+export default function SecondaryButton({ children, className, onClick, ...props }) {
+  const cx = classNames([
+    "border border-primary text-primary hover:bg-primary hover:text-white transition-colors duration-300 rounded-full py-3 px-6 md:py-4 md:px-10",
+    className
+  ]);
+
+  return (
+    <button
+      onClick={onClick}
+      className={cx}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 }
