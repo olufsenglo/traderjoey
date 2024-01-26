1fe259e3467b381bb5f20ef69318de4b68db1ac2
Glo <olsheaolufsen@gmail.com>
2024-01-26 20:44:39 +0100

    start crypto section

---
+++ src/components/sections/CryptocurrencySection.jsx
@@ -0,0 +1,7 @@
+export default function CryptocurrrencySection() {
+    return (
+        <section className="-mt-8">
+            Section 2
+        </section>
+    )
+}
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,10 +1,12 @@
 import Layout from "../components/Layout";
 import HeroSection from "../components/sections/HeroSection";
+import CryptocurrrencySection from "../components/sections/CryptocurrencySection";
 
 export default function Index() {
     return (
         <Layout>
             <HeroSection />
+            <CryptocurrrencySection />
         </Layout>
     )
 }
