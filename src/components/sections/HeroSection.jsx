8ebeda45276f04d9a2b02a18217238c4a3a57f1d
Glo <olsheaolufsen@gmail.com>
2024-01-26 20:30:44 +0100

    Add buttons

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -1,10 +1,30 @@
+import globe from '../../images/illustrations/globe.png';
+import PrimaryButton from '../buttons/PrimaryButton';
+import SecondaryButton from '../buttons/SecondaryButton';
+
 export default function HeroSection() {
     return (
         <section>
             <div className="grid grid-cols-2">
-                <div></div>
+                <div className='flex items-center'>
+                    <div>
+                        <p className="text-primary">SIGN UP</p>
+                        <h1 className="text-4xl font-bold">The Worlds</h1>
+                        <h1 className="text-4xl font-bold"> most reliable</h1>
+                        <h1 className="text-4xl font-bold"> trading web app</h1>
+                        <p className="text-gray">Trade, buy and sell local and international currencyy convenient <br /> trusted at the lowest rate</p>
+                        <div className="flex">
+                            <PrimaryButton>
+                                Get Started
+                            </PrimaryButton>
+                            <SecondaryButton>
+                                Download App
+                            </SecondaryButton>
+                        </div>
+                    </div>
+                </div>
                 <div>
-                    <img src="" alt="" />
+                    <img src={globe} alt="Globe" />
                 </div>
             </div>
         </section>
