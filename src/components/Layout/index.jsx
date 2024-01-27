463120c471b8e8dc5ec57f3e7b3db52f0321a862
Glo <olsheaolufsen@gmail.com>
2024-01-27 09:13:53 +0100

    Add coinrow and listcoin to crypto section

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,10 +1,12 @@
 import Navbar from '../Navbar';
+import Footer from "../Footer";
 
 export default function Layout({ children }) {
     return (
         <div className="container mx-auto">
             <Navbar />
             {children}
+            <Footer />
         </div>
     )
 }
