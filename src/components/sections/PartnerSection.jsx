af0ad93930cdc5b0042240680d014391773cacbf
Glo <olsheaolufsen@gmail.com>
2024-01-27 09:31:57 +0100

    Start partner section

---
+++ src/components/sections/PartnerSection.jsx
@@ -0,0 +1,7 @@
+export default function PartnerSection() {
+    return (
+        <div>
+            Partner
+        </div>
+    )
+}
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,12 +2,16 @@ import Layout from "../components/Layout";
 import HeroSection from "../components/sections/HeroSection";
 import CryptocurrrencySection from "../components/sections/CryptocurrencySection";
 import Footer from "../components/Footer";
+import BuyAndTradeSection from "../../../../nefa/src/components/sections/BuyAndTradeSection";
+import PartnerSection from "../components/sections/PartnerSection";
 
 export default function Index() {
     return (
         <Layout>
             <HeroSection />
             <CryptocurrrencySection />
+            <BuyAndTradeSection />
+            <PartnerSection />
             <Footer />
         </Layout>
     )
