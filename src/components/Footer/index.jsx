f9f7bd475ecc4afd1d7adda0f38207566a4e74dc
Glo <olsheaolufsen@gmail.com>
2024-01-27 12:57:13 +0100

    back to footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,15 @@
 export default function Footer() {
     return (
         <footer>
-            Footer
+            <div className="container px-2 border-y border-lightgray py-6">
+                <div className="grid lg:grid-cols-3 xl:grid-cols-4">
+                    <div className="p-6 xl:flex md:justify-between xl:border-r border-lightgray">
+                        <div className='mb-6'>
+                            Footer
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
     )
 }
